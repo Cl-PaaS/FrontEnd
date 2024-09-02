7c6d7d29c18f0a5adf4cf7add7061474a62baf1a
dayeon1201 <dydid1201@gmail.com>
2024-09-02 23:04:59 +0900

    Fix textview and button in layout

--- app/src/main/java/com/example/clpaas_frontend/data/RetrofitService.java
+++ app/src/main/java/com/example/clpaas_frontend/data/RetrofitService.java
@@ -7,10 +7,6 @@ public interface RetrofitService {
     // 데이터 전달 및 값 받아오기
     @POST("api/message")
     Call<ResponseData> requestData(@Body RequestData data);
-<<<<<<< HEAD
-}
-=======
     // 나중에 여기를 수정해야함 ( api 경로)
 
 }
->>>>>>> origin/feat/#1
